58c6324f9dc5695b4e1a888d01d44530f5499125
Kiiza Atwiine Stephen <stephenkiiza123@gmail.com>
2022-11-10 15:18:59 +0300

    Added some controllers

---
+++ models/notes.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const notesSchema = new mongoose.Schema({
+
+  title: {
+    type: 'String',
+    required: true
+  },
+
+  author: {
+    type: 'String',
+    required: true
+  },
+
+  content: {
+    type: 'String',
+    required: true
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('notes', notesSchema);
