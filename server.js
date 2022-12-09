e6fdeccaf93b3a26b9acf6a69514718b486a935a
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2022-12-09 14:59:01 +0000

    Added Register and Login Forms

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,7 +19,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public/')));
 
 app.use('/', require('./routes/notes.route'));
+app.use("/", require("./routes/user.route"));
 
 app.listen(PORT, () => {
-  console.log(`Server Running on http://localhost:${PORT}`)
+  console.log(`Server Running on http://localhost:${PORT}`);
 })
